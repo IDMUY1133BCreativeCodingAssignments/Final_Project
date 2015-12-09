30fc0adef9ddf82f24ebcd1a966c6dc6d038db9e
binkobon <kc2605@nyu.edu>
2015-12-09 01:00:27 -0500

    Slight progress
    Having some trouble getting the character class to work, even though I'm
    referencing this directly from the Khan Academy example.

--- KChen_FinalProj/sketch.js
+++ KChen_FinalProj/sketch.js
@@ -1,26 +1,78 @@
-var samplebg;
-var x;
-var xpos;
-var ypos;
+/* This code is made referencing from Khan Academy's 'Forest Environment'
+example. : 
+https://www.khanacademy.org/computing/computer-programming/programming-games-visualizations/side-scroller/a/forest-environment */
+
+//function Character(x, y){
+//    this.x = x;
+//    this.y = y;
+//    this.img = sprite;
+//}
+//
+//Character.prototype.display = function(){
+//    image(this.img, this.x, this.y, 50, 50);
+//}
+//
+//Character.prototype.jump = function(){
+//    this.y -= 5;
+//}
+//
+//Character.prototype.fall = function(){
+//    this.y += 5;
+//}
+
+var tile;
+var ground = [];
+
+//var sprite;
+//var sample = new Character(50, 50);
+
+var scene1;
+var scenery = [];
 
 
 function preload(){
-    samplebg = loadImage("data/samplebg.png");
+//    sprite = loadImage("data/sprite.png"); //loads a 50x40 pixel image
+    tile = loadImage("data/groundtile.png"); // loads a 50x29 pixel image
+    scene1 = loadImage("data/bg1.png"); //loads a 146x350 pixel image
+    
+    for (var i = 0; i < 11; i++){
+        ground.push(i*50);
+    }
+    
+    for (var i = 0; i < 5; i++){
+        scenery.push(i*146);
+    }
 }
 
 function setup(){
-    createCanvas(800, 500);
-    background(200, 200, 250);
+    createCanvas(500, 350);
+    background(220, 220, 255);
 }
 
 function draw(){
-    x = 0;
-    image(samplebg, x, 0, 10000, 300);
-    character();
-}
+//    background(200, 220, 255);
+        
+    for (var i = 0; i < scenery.length; i++){
+       image(scene1, scenery[i], 0); 
+        
+        scenery[i] -=1;
+        
+        if (scenery[i] <= -146){
+            scenery[i] = 584;
+        }
+    }
+    
+    
+    for (var i = 0; i < ground.length; i++){
+        image(tile, ground[i], 320);
+        
+        ground[i] -= 1;
+        
+        if (ground[i] <= -50){
+            ground[i] = width;
+        }
+    }
 
-function character(){
-    xpos = 25;
-    ypos = 275;
-    ellipse(xpos, ypos, 50, 50);    
+    ellipse(50, 300, 50, 50);
+//    sample.display();
 }
