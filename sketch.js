9f204d5160807c07d5dda0b9d9feb37be827868b
sharonhlee <sl4551@nyu.edu>
2015-12-11 17:17:56 -0500

    little trouble with updating

--- sketch.js
+++ sketch.js
@@ -1,48 +1,50 @@
-var startpos=300;//start character at the middle of screen
-var x;
-var yspeed=6;
-var clouds=[];
-var cloudpos=-75;
+var pos = 10;
+var sky;
+var user;
 var enemies;
-var enemy1pos=-100;
 var score=0;
-var x;
-var newEnemy=false;
+var userImg,enemyImg,cloudsImg;
+var gameover;
 
 function setup(){
 	createCanvas(600,600);
     
     userImg=loadImage("data/balloons-36236_960_720.png");
     enemyImg=loadImage("data/9cprBLgcE.png");
+    cloudsImg=loadImage("data/bg.png");
     
-    user=createSprite(x,600,50,50);
-    user.addImage(userImg);
+    user=createSprite(width/2,500,50,50);
+    //user.addImage(userImg);
+    user.shapeColor=color(255);
     user.velocity.y=-6
     
-    rectMode(CENTER);
-    for(var i=0;i<4;i++){
-        clouds[i]=new Cloud(cloudpos,yspeed);//array of cloud objects
-        cloudpos-=150;
-    }//close for
+    sky=createSprite(width/2,height/2,width,height);
+    sky.addImage(cloudsImg);
+        //img 500x362
+    
     enemies= new Group();
-//    textFont("Georgia",52);
+    
+    camera.position.x=width/2;
+    
+    updateSprites(true);
 }//close setup
 
 function draw(){
-    background(0,120,130);
-    noStroke();
-    x=constrain(startpos,25,width-25);//keep character within bounds
-    for (var i=0;i<clouds.length;i++){//loop through cloud objects
-        clouds[i].move();
-        clouds[i].display();
-        score+=1;
-    }//close for loop
-    //console.log(score);
-//    fill(0,0,255);
-//    text(score,100,100);
-//    fill(255);
-//    rect(x,500,50,50);//user character
-    if (score%200===0){
+    background(182,237,252);
+    
+    if (keyDown(LEFT_ARROW)){
+        user.position.x-=pos;
+    }
+    if (keyDown(RIGHT_ARROW)){
+        user.position.x+=pos;    
+    }
+    if(user.position.x<0){//aka constraining to keep user within width bounds
+        user.position.x=user.width/2;
+    }
+    if(user.position.x>width){
+        user.position.x=width-user.width/2;
+    }
+    if (frameCount%70===0){
         var enemy=createSprite(random(width),user.position.y-height,50,50);
         enemy.addImage(enemyImg);
         enemies.add(enemy);
@@ -52,63 +54,16 @@ function draw(){
             enemies[i].remove();
         }//close if
     }//close for
+    console.log(user.position.x);
+    camera.position.y=user.position.y-100;
+    
+    //sky vertical scroll
+    if (camera.position.y<sky.position.y-100-sky.height){
+        sky.position.y=-sky.height;
+    }
     
     drawSprites(enemies);
-    drawSprites(user);
+    drawSprite(user);
+    drawSprite(sky);
+    ;
 }//close draw
-
-function keyPressed(){
-    if (keyCode === LEFT_ARROW){//user control to steer character
-        startpos-=10//left, right arrows steer
-    }//close left if
-    if (keyCode === RIGHT_ARROW){
-        startpos+=10;
-    }//close right if
-}
-//vertical scroll
-function Cloud(cloudpos,yspeed){
-    this.xpos=random(width);
-    this.ypos=cloudpos;
-    this.cloudhe=75;
-    this.cloudwi=100;
-    this.speed=yspeed;//clouds go down, illusion of character going up
-}//close cloud
-
-Cloud.prototype.move=function(){
-    if(this.ypos>height+this.cloudhe/2){//start again from top
-        this.ypos=-this.cloudhe/2;
-    }//close ypos if
-    this.ypos+=this.speed;
-}//cloud move method
-
-Cloud.prototype.display=function(){
-    fill(255,100);
-    rect(this.xpos,this.ypos,this.cloudwi,this.cloudhe);//temp rect
-}//close display method
-
-function Enemy1(enemy1pos,yspeed,show){
-    this.xpos=random(width);
-    this.ypos=enemy1pos;
-    this.speed=yspeed;
-    this.enemy1he=50;
-    this.enemy1wi=50;
-    this.show=show;
-}//close enemy1
-
-Enemy1.prototype.move=function(){
-    this.ypos+=this.speed;
-    if (this.ypos>height+this.enemy1he/2){
-        this.show=false;
-    }
-}
-
-Enemy1.prototype.display=function(){
-    fill(255,0,0);
-    rect(this.xpos,this.ypos,this.enemy1he,this.enemy1wi);//temp rect
-}//close display method
-
-
-
-
-
-
