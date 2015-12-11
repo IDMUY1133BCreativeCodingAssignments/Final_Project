a5b153da3aafc8d82e8721dd58916ef5f6d47a63
sharonhlee <sl4551@nyu.edu>
2015-12-11 14:37:10 -0500

    update with p5 play lib
    please hold until i learn how to work this lib…

--- sketch.js
+++ sketch.js
@@ -1,28 +1,60 @@
 var startpos=300;//start character at the middle of screen
 var x;
+var yspeed=6;
 var clouds=[];
-var cloudpos=-75
+var cloudpos=-75;
+var enemies;
+var enemy1pos=-100;
+var score=0;
+var x;
+var newEnemy=false;
 
 function setup(){
-    frameRate=240;
 	createCanvas(600,600);
+    
+    userImg=loadImage("data/balloons-36236_960_720.png");
+    enemyImg=loadImage("data/9cprBLgcE.png");
+    
+    user=createSprite(x,600,50,50);
+    user.addImage(userImg);
+    user.velocity.y=-6
+    
     rectMode(CENTER);
     for(var i=0;i<4;i++){
-        clouds[i]=new Cloud(cloudpos);//array of cloud objects
+        clouds[i]=new Cloud(cloudpos,yspeed);//array of cloud objects
         cloudpos-=150;
     }//close for
+    enemies= new Group();
+//    textFont("Georgia",52);
 }//close setup
 
 function draw(){
-    background(50,100,100);
+    background(0,120,130);
     noStroke();
-    fill(255);
     x=constrain(startpos,25,width-25);//keep character within bounds
     for (var i=0;i<clouds.length;i++){//loop through cloud objects
         clouds[i].move();
         clouds[i].display();
-    rect(x,500,50,50);//user character
+        score+=1;
     }//close for loop
+    //console.log(score);
+//    fill(0,0,255);
+//    text(score,100,100);
+//    fill(255);
+//    rect(x,500,50,50);//user character
+    if (score%200===0){
+        var enemy=createSprite(random(width),user.position.y-height,50,50);
+        enemy.addImage(enemyImg);
+        enemies.add(enemy);
+    }
+    for (var i = 0; i<enemies.length;i++){
+        if(enemies[i].position.y>height+25){
+            enemies[i].remove();
+        }//close if
+    }//close for
+    
+    drawSprites(enemies);
+    drawSprites(user);
 }//close draw
 
 function keyPressed(){
@@ -32,13 +64,14 @@ function keyPressed(){
     if (keyCode === RIGHT_ARROW){
         startpos+=10;
     }//close right if
-}//vertical scroll
-function Cloud(cloudpos){
+}
+//vertical scroll
+function Cloud(cloudpos,yspeed){
     this.xpos=random(width);
     this.ypos=cloudpos;
     this.cloudhe=75;
     this.cloudwi=100;
-    this.speed=3;//clouds go down, illusion of character going up
+    this.speed=yspeed;//clouds go down, illusion of character going up
 }//close cloud
 
 Cloud.prototype.move=function(){
@@ -52,6 +85,28 @@ Cloud.prototype.display=function(){
     fill(255,100);
     rect(this.xpos,this.ypos,this.cloudwi,this.cloudhe);//temp rect
 }//close display method
+
+function Enemy1(enemy1pos,yspeed,show){
+    this.xpos=random(width);
+    this.ypos=enemy1pos;
+    this.speed=yspeed;
+    this.enemy1he=50;
+    this.enemy1wi=50;
+    this.show=show;
+}//close enemy1
+
+Enemy1.prototype.move=function(){
+    this.ypos+=this.speed;
+    if (this.ypos>height+this.enemy1he/2){
+        this.show=false;
+    }
+}
+
+Enemy1.prototype.display=function(){
+    fill(255,0,0);
+    rect(this.xpos,this.ypos,this.enemy1he,this.enemy1wi);//temp rect
+}//close display method
+
 
 
 
