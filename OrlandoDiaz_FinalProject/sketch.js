b53b87d3fb0ff00b7e9a55e66f8900ec758d18dc
od397 <od397@nyu.edu>
2015-12-13 00:27:19 -0500

    OrlandoDiaz_FinalProjectupdate
    So I got a lot of stuff working! Yay! But the problem I’m having is
    with the healthbar and health counters, I point out the line and
    comment on what’s happening in the code and would appreciate help in
    that area. And how would you go about making it so that the boss and
    hero take turns attacking? It’s boggling my mind trying to comprehend
    how to put that into code. Also, I added a little intro movement and
    animation for my monster. Enjoy the music too! And speaking of the
    music, how do I make it so that when you press “z” it doesn’t play the
    song again when you press “z” again? That’s my current situation right
    now.

--- OrlandoDiaz_FinalProject/sketch.js
+++ OrlandoDiaz_FinalProject/sketch.js
@@ -1,57 +1,153 @@
+//The Nature of Code by Daniel Shiffman - http://natureofcode.com
 //Ok I'm getting the animations done first since math scares me for this, any tips for the upcoming math-based code?
-var hearticon;
-var skullicon;
-var sfearstart;
-var bosstheme;
-var sfearx = 300;
-var sfeary = 150;
-var floatlimit = 170;
-var speed = 1;
-var speed2 = 15;
-var pixel;
+//on line 70 is my primary problem with the boss health
+var hearticon; //heart
+var skullicon; //skull
+var battlebackground; //trippy background
+var sfearstart; //the first stage of the boss
+var sfearpissed; //the 2nd phase of the boss
+var bosstheme; //unused other music
+var sfearx = 300; //boss's x position
+var sfeary = 150; //boss's y position
+var floatlimit = 270; // what keeps the boss moving up and down
+var speed = 1; //boss's vertical speed
+var speed2 = 15; //boss's other vertical speed
+var pixel; //pixel font
+var bosshploss = 0; //gray "health loss" bar
+var bhealth = "10/10"; //numerical boss hp value
+var yourhp = "10/10"; //numerical hero hp value
+var opening = "SFEAR attacks!"; //first text before battle
+var pressthistofight = "Press 'z' to battle!" //instructions to start battle text
+var fight = "FIGHT"; //access to fight command text
+var magic = "MAGIC"; //access to magic command text
+var item = "ITEM"; //access to items text
+var position; //horizontal positioning
+var velocity; //horizontal speed
+var acceleration; //what keeps him moving side to side consistently
+var theme2; //other unused theme
+var asgore1; //current boss theme
+var asgore2; //2nd phase current boss theme
+var battle = false; //battle start detector boolean
+var backdrop = 255; //background variable
+var fightx; //distance from FIGHT needed to click to activate
+var magicx; //distance from MAGIC needed to click to activate
+var itemx; //distance from ITEM needed to click to activate
+var fightcolor = 255; //text color
+var magiccolor = 255; //text color
+var itemcolor = 255; //text color
+var fightdetector1 = false;
+var fightdetector2 = false;
 
 function preload(){
   hearticon = loadImage('data/heart.png');
   skullicon = loadImage('data/pixelskull.png');
-  sfearstart = loadImage('data/Sfear0001.png'); //yes the boss monster is named "Sfear", genius right?
-  bosstheme = loadSound('data/toby fox - UNDERTALE Soundtrack - 87 Hopes and Dreams.mp3');
+  battlebackground = loadImage('data/Sfearbattle.png');
+  sfearstart = loadAnimation("data/Sfear0001.png"); //yes the boss monster is named "Sfear", genius right?
+  bosstheme = loadSound('data/toby fox - UNDERTALE Soundtrack - 79 Your Best Nightmare.mp3');
   pixel = loadFont('data/pixel.otf');
+  theme2 = loadSound('data/toby fox - UNDERTALE Soundtrack - 80 Finale.mp3');
+  asgore1 = loadSound('data/toby fox - UNDERTALE Soundtrack - 76 Bergentrückung.mp3');
+  asgore2 = loadSound('data/toby fox - UNDERTALE Soundtrack - 77 ASGORE.mp3');
+  sfearpissed = loadAnimation('data/Sfear0001.png', 'data/Sfear0010.png');
 }
 
 function setup(){ //setup is apparently where music comes from
   createCanvas(800,600);
   frameRate(60);
   smooth();
+  keyTyped();
   music();
+  position = createVector(-100, 0);
+  velocity = createVector(1, 0);
+  acceleration = createVector(1, 0);
 }
 
 function draw(){
-  background(255);
+  background(battlebackground);
+  fightoption();
   allthestuffsodrawcanlookniceandclean();
+  if(fightdetector1 = true){
+  	if(mouseIsPressed){
+  		bhealth = "8/10"; //I have a problem here, when I click the health just skips to 8/10 instead of going to 9/10 first.
+  		bosshploss = 50; // (read above first) I assume that when mouse is pressed, it simultaneously triggers the 9 and 8hp. Any tips for this problem?
+  		fightdetector2 = true;
+  	}//mouseIsPressed
+  }//fightdetector1 is true
+   if(fightdetector2 = true){
+  	if(mouseIsPressed){
+      fightcolor = 255;
+  		bhealth = "7/10";
+  		bosshploss = 75;
+  		sfearstart = sfearpissed;
+  		sfearpissed.looping = false;
+  		//if(sfearpissed.getFrame() == sfearpissed.getLastFrame()){
+		//sfearpissed.stop();
+		//}//animation stop
+  	}//mouseIsPressed
+  }//fightdetector1 is true
 }
 
 function allthestuffsodrawcanlookniceandclean(){
   boss();
+  bossmoving();
   topbox();
   bottombox();
   herohealth();
   bosshp();
   icons();
-  keyTyped();
   allthetext();
 }
 
-function music(){ //Look up Undertale, it has awesome music! I'll probably have this change to another song after the boss is low on HP.
-	bosstheme.setVolume(0.1);
-	bosstheme.play();
-	bosstheme.loop();
+function fightoption(){
+  fill(255, 0);
+  fightx = dist(mouseX, mouseY, 205, 85);
+  if(mouseIsPressed){
+  	if(fightx < 110){
+  	fightcolor = 100;
+  	fightdetector1 = true;
+  	bosshploss = 25; //"lower" the boss's visual health
+	bhealth = "9/10"; //lower the boss's numerical health
+  	} //FIGHT input on menu
+  }//if the mouse is pressed
+}//fightoption
+
+function bossmoving(){
+  acceleration.add(0.2);
+  velocity.add(acceleration);
+  position.add(velocity);
+  acceleration.mult(0);
+      if (position.x > 530) { //so Sfear stays near the middle area of the screen
+      position.x = 530;
+      velocity.x *= -1;  // moves left!
+    } else if (position.x < 170) {
+      velocity.x *= -1; //moves right!
+      constrain(velocity, 0, 5);
+      position.x = 170;
+    }
 }
 
-function keyTyped(){ //if for some weird reason you have awesome music by toby fox you can click this.
-	if(key == 'm' || key == 'M'){
-		bosstheme.stop();
-	}
-}
+function music(){ //Look up Undertale, it has awesome music! I'll probably have this change to another song after the boss is low on HP.
+	asgore1.setVolume(0.2);
+	asgore1.play();
+	asgore1.loop();
+}//music
+
+function keyTyped(){ 
+	if(key == 'm' || key == 'M'){//if for some weird reason you hate awesome music you can click this.
+		asgore1.stop();
+		asgore2.stop();
+	}//press "m" to mute the music
+	if(key == 'z' || key == 'Z'){//starts the fight
+		opening = ' ';
+		pressthistofight = ' ';
+		asgore1.stop();
+		asgore2.setVolume(0.2);
+		asgore2.play();
+		asgore2.loop();
+		backdrop = battlebackground;
+		battle = true;
+		}//if "Z" is pressed, the fight begins!
+}//keyTyped
 
 function topbox(){
   fill(0);
@@ -80,7 +176,13 @@ function bosshp(){ //I'll probably make a black rectangle to cover up the red on
   fill(255, 0, 0);
   strokeWeight(2);
   stroke(0);
-  rect(420, 380, 250, 30);
+  rect(420, 380, 240, 30);
+  fill(100);
+  rect(420, 380, bosshploss, 30);
+  //bosshploss = bosshploss + 10;
+  if(bosshploss >= 250){
+  	bosshploss = bosshploss - 10;
+  }
   noFill();	
   noStroke();
 }
@@ -91,42 +193,61 @@ function icons(){ //lol these don't change at all.
 }
 
 function boss(){ //Ok so I spent forever trying to get him to float, now I can probably do the same for horizontal movement.
-	image(sfearstart, sfearx, sfeary);
-	 if(sfeary <= floatlimit){
+	animation(sfearstart, position.x, sfeary);
+	 if(sfeary <= floatlimit){ //these 2 if statements move Sfear up and down
 			sfeary += speed;
-		}
+		}//if
 	if(sfeary >= floatlimit){
 			sfeary -= speed2;
-		}
-		//else{
-		//	sfeary = floatdown;
-		//}
-		//if(sfeary <= 130){
-		//	sfeary = sfeary + 1;
-		//}
-}
+		}//if
+}//boss
 
 function allthetext(){ //So this is where all the text-based code will occur, including the letter-by-letter formation of the code and the number values.
 	fill(255);
+	textFont(pixel, 20);
+	text(pressthistofight, 100, 120);
 	textFont(pixel, 30);
-	text("SFEAR attacks!", 100, 80);
-	//text("40/40") this is what it'll say for the boss's hp, it will be similar for the hero. How would I get the numbers to decrease by "hit", as in how would I change the numbers in text.
+	text(opening, 100, 80);
+	if(battle == true){
+		textFont(pixel, 30);
+	fill(fightcolor);
+		text(fight, 150, 90);
+	fill(magiccolor);
+		text(magic, 350, 90);
+	fill(itemcolor);
+		text(item, 550, 90);
+	}
+	text(yourhp, 120, 463);
+	text(bhealth, 300, 405); //this is what it'll say for the boss's hp, it will be similar for the hero. How would I get the numbers to decrease by "hit", as in how would I change the numbers in text.
 }
 
+//use this for animation reference later on
+//  if(explode.getFrame()==explode.getLastFrame())
+//  explode.stop();
 
+  //pos.add(bossspeed);
+  //pos.x++;
+  //if(pos.x > (windowWidth)){
+  	//bossspeed.x = bossspeed.x * -1;
+  	//pos.x = 0;
+  //}
+  //bossspeed.x = movingboss;
+	/*if(pos.x >= 400){
+		bossspeed.x = bossspeed.x - movingboss;
+	}
+	//if(pos.x > 401){
+	//	pos.x = 400;
+	//}
+	if(pos.x <= 200){
+		bossspeed.x = bossspeed + movingboss;
+	}
+	//var pos = constrain(pos.x, 200, 400);
+	*/
+  	//pos = createVector(300, 150);
+  	//movingboss = 1.0;
+  	//bossspeed = createVector();
 
-
-
-
-
-
-
-
-
-
-
-/*
-var rust;
+/*var rust;
 var script;
 var font1;
 var myString;
@@ -172,5 +293,4 @@ function draw(){
 	}
 	//console.log(x);
 	framecount = (framecount+1) % 60
-}
-*/
+}*/
